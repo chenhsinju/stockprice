964d9786e7acae92b14f00266ae23954cdb75bf2
ccwsl <n574838382000@gmail.com>
2022-04-21 15:57:17 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template
+import csv
+import requests
+from stock import Stock
+
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+
+# 站內要顯示的個股列表
+stock_list = []
+
+
+# 打開stock_list.csv
+with open('stock_list.csv', newline='', encoding='utf-8') as stock_csv:
+    # 把csv檔案格式轉換為list
+    table = list(csv.reader(stock_csv))
+    # print(table)
+    for t in table:
+        # 建立一個股物件
+        s = Stock(t[0], t[1])
+        # 將個股物件新增到個股清單內
+        stock_list.append(s)
+        # print(s)
+
+
+@app.route('/')
+# 首頁路由
+def index_page():
+    return render_template('index.html',
+                           stock_list=stock_list)
+
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
+
+@app.route('/stock/<sid>')
+# 個股詳情頁路由
+def stock_detail_page(sid):
+    stock = None
+    for s in stock_list:
+        if s.id == sid:
+            stock = s
+            # print(stock)
+    # 爬取個股詳細資料
+    stock.get_data()
+    return render_template('stock-detail.html',
+                           stock=stock)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ stock.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class Stock:
+    # 個股包含以下屬性
+    # 個股代號、名稱、成交價
+    def __init__(self, sid, name):
+        self.id = sid
+        self.name = name
+        self.price = None
+
+    # 定義爬取個股資訊的函式
+    def get_data(self):
+        url = 'https://tw.stock.yahoo.com/q/q?s=' + self.id
+        response = requests.get(url=url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        # 找到你要的那個div 其下的span標籤, 並擷取標籤裡的文字
+        name = soup.find('div', 'D(f) Ai(fe) Mb(4px)').span.text.strip()
+        self.price = name
