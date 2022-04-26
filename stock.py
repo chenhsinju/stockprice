dd56dc1f3b825a6696392c27d8e1ed80ed738b65
ccwsl <n574838382000@gmail.com>
2022-04-26 16:51:42 +0800

    stock_price.csv

--- stock.py
+++ stock.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-import pymysql
+import csv
 
 
-class Stock:
+class Stock():
     # 個股包含以下屬性
     # 個股代號、名稱、成交價
     def __init__(self, sid, name):
@@ -18,21 +18,8 @@ class Stock:
         soup = BeautifulSoup(response.text, 'lxml')
         # 找到你要的那個div 其下的span標籤, 並擷取標籤裡的文字
         price = soup.find('div', 'D(f) Ai(fe) Mb(4px)').span.text.strip()
+        # print(type(price))
         self.price = price
 
-    def save(self):
-        db_settings = {
-            'host': 'us-cdbr-east-05.cleardb.net',
-            'port': 3306,
-            'user': 'b06fddb9902609',
-            'password': 'd0624155',
-            'db': 'heroku_d65d0f6f8283b18',
-            'charset': 'utf8'
-        }
-        try:
-            conn = pymysql.connect(**db_settings)
-            with conn.cursor() as cursor:
-                cursor.execute(self.price)
-                conn.commit()
-        except Exception as ex:
-            print('Exception: ', ex)
+        with open('stock_price.csv', 'w') as stock_price_csv:
+            stock_price_csv.write(price)
