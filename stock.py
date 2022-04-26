c12a1aa11349334ebbc4cd3693b140ea8de4e132
ccwsl <n574838382000@gmail.com>
2022-04-26 17:14:17 +0800

    make it better

--- stock.py
+++ stock.py
@@ -13,11 +13,12 @@ class Stock():
 
     # 定義爬取個股資訊的函式
     def get_data(self):
-        url = 'https://tw.stock.yahoo.com/q/q?s=' + self.id
-        response = requests.get(url=url)
-        soup = BeautifulSoup(response.text, 'lxml')
-        # 找到你要的那個div 其下的span標籤, 並擷取標籤裡的文字
-        price = soup.find('div', 'D(f) Ai(fe) Mb(4px)').span.text.strip()
+        # url = 'https://tw.stock.yahoo.com/q/q?s=' + self.id
+        # response = requests.get(url=url)
+        # soup = BeautifulSoup(response.text, 'lxml')
+        # # 找到你要的那個div 其下的span標籤, 並擷取標籤裡的文字
+        # price = soup.find('div', 'D(f) Ai(fe) Mb(4px)').span.text.strip()
+        price = '1000'
         # print(type(price))
         self.price = price
 
